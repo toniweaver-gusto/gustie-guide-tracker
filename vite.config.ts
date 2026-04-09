04258720039677655e98e60409f21c4ecac5a4fb
Your Name <you@example.com>
2026-04-09 16:51:40 -0600

    fix browser caching for deployments

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,13 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: { "@": path.resolve(__dirname, "src") },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
+  },
 }));
